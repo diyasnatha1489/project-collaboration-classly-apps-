8f7831c39057bd37aee590f646297aaf1f7fbaf7
indanamaula276 <indanamaula276@gmail.com>
2026-05-22 11:03:07 +0700

    Update: Profile

--- admin/profile.php
+++ admin/profile.php
@@ -4,9 +4,44 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Profile Page</title>
+    <link rel="stylesheet" href="../css/profile.css">
 </head>
 <body>
-    <a href="logout.php">Log Out</a>
+    <div class="profile-container">
+        <div class="header">
+            <div class="back-container">
+                <span class="back">&#8592;</span>
+                <h2>Account</h2>
+            </div>
+            <div>
+                <a href="logout.php">Log Out</a>
+            </div>
+        </div>
+
+        <div class="content">
+            <div class="profile">
+                <img src="../picture/user-profile.jpg" alt="profile">
+                <div class="edit-icon">Edit</div>
+            </div>
+
+            <div class="card">
+                <div class="item">
+                    <span>Maula Qodri Lail</span>
+                    <span class="edit">Edit</span>
+                </div>
+                <div class="item">
+                    <span>maulaqodri6767@gmail.com</span>
+                    <span class="edit">Edit</span>
+                </div>
+                <div class="item">
+                    <span>*****************</span>
+                    <span class="edit">Edit</span>
+                </div>
+            </div>
+        </div>
+
+        
+    </div>
 </body>
 </html>
 
